b5d745cf727a6ae8fda095f85cc7811b295c7844
Darshita <131277268+darshita27@users.noreply.github.com>
2026-05-12 13:27:23 +0530

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static bool compare(const pair<int,int>&p1, const pair<int,int>&p2){
+        if(p1.second==p2.second){
+        return p1.first > p2.first;
+        }
+        return p1.second> p2.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<pair<int,int>> freq(mp.begin(),mp.end());
+        sort(freq.begin(), freq.end(), compare);
+        for(int i=0;i<k;i++){
+            res.push_back(freq[i].first);
+        }
+        return res;
+
+    }
+};

e138b2e7358fbd20f22dd6cdcd2eadfa92441384
Darshita <131277268+darshita27@users.noreply.github.com>
2026-05-12 12:27:11 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+string generate(string &word){
+    int arr[26]={0};
+    for(char &ch:word){
+        arr[ch-'a']++;
+    }
+    string new_word;
+    for(int i=0;i<26;i++){
+        int freq= arr[i];
+        new_word+= string (freq, i+'a');
+    }
+    return new_word;
+}
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>res;
+        unordered_map<string, vector<string>>mp;
+       int n=strs.size();
+       for(int i=0;i<n;i++){
+        string word= strs[i];
+        string new_word= generate(word);
+        mp[new_word].push_back(word);
+       } 
+       for(auto &it:mp){
+        res.push_back(it.second);
+       }
+       return res;
+    }
+};
